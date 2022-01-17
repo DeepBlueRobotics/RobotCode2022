950abdcd3157bc3777b299104ef2397a9bbbd2c9
ProfessorAtomicManiac <cyific@gmail.com>
2022-01-17 14:23:05 -0800

    Implementing Color Sensor

--- src/main/java/org/team199/robot2022/RobotContainer.java
+++ src/main/java/org/team199/robot2022/RobotContainer.java
@@ -6,6 +6,8 @@ package org.team199.robot2022;
 
 import org.team199.robot2022.commands.TeleopDrive;
 import org.team199.robot2022.subsystems.Drivetrain;
+import org.team199.robot2022.subsystems.ColorSensor;
+import org.team199.robot2022.commands.DetectColor;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Joystick;
@@ -31,10 +33,14 @@ public class RobotContainer {
   public final Drivetrain dt = new Drivetrain();
   public final PowerDistribution pdp = new PowerDistribution();
 
+  public final ColorSensor colorSensor = new ColorSensor();
+
   /**
    * The container for the robot. Contains subsystems, OI devices, and commands.
    */
   public RobotContainer() {
+    configureColorSensor();
+
     if (DriverStation.isJoystickConnected(Constants.OI.LeftJoy.port)) {
       configureButtonBindingsLeftJoy();
     } else {
@@ -57,6 +63,10 @@ public class RobotContainer {
         () -> inputProcessing(getStickValue(Constants.OI.StickType.RIGHT, Constants.OI.StickDirection.Y)),
         () -> inputProcessing(getStickValue(Constants.OI.StickType.RIGHT, Constants.OI.StickDirection.X)),
         () -> inputProcessing(getStickValue(Constants.OI.StickType.LEFT, Constants.OI.StickDirection.X))));
+  }
+
+  public void configureColorSensor() {
+    colorSensor.setDefaultCommand(new DetectColor(colorSensor)); 
   }
 
   private void configureButtonBindingsLeftJoy() {
