d418ecea31adcd549bec2d7e21c0eee6e15ed374
Nick <807632@seq.org>
2022-01-19 17:59:42 -0800

    skeleton

--- src/main/java/org/team199/robot2022/commands/DetectColor.java
+++ src/main/java/org/team199/robot2022/commands/DetectColor.java
@@ -18,17 +18,39 @@ public class DetectColor extends CommandBase {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    /**
+     * may want to add WHAT COLOR BALLS THE ROBOT WANTS TO HAVE
+     * change the boolean output for false to match the correct color
+     */
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
     colorSensor.detectColor();
+    /**
+     * once the color is read, it will output to the smartdashboard
+     * we can then use this information and put it into regurgitate
+     * 
+     * - create a boolean that detects if a ball is "correct", this boolean 
+     * can then be switched around depending on what team we are.
+     *    - ideally, we want the ball to be instantly regurgitated if it is not
+     *      the "correct" color
+     *    
+     * 
+     * 
+     */
   }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    /**
+     * I think we want this command to end when the robot stops running
+     * so we will have the sensor running from start of auto to end of teleop
+     */
+  }
 
   // Returns true when the command should end.
   @Override
