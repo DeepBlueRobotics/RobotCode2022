68b57fcdf6f10acb9e1d9e5528fa4881668da057
0sani <jackahansen18@gmail.com>
2022-01-29 15:09:33 -0800

    improved skeleton code

--- src/main/java/org/team199/robot2022/commands/Shoot.java
+++ src/main/java/org/team199/robot2022/commands/Shoot.java
@@ -2,6 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+//Shoots one ball (is called with while pressed AND when pressed)
 package org.team199.robot2022.commands;
 
 import org.team199.robot2022.Constants;
@@ -25,6 +26,7 @@ public class Shoot extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    //set initial # of balls in feeder
   }
 
   /** Automatically checks what ball is in the shooter. 
@@ -32,21 +34,21 @@ public class Shoot extends CommandBase {
     * If its right, it can automatically shoot.
     */
   @Override
+  //Shoots a ball forward/backwards depending on ball color
   public void execute() {
-    /**
-     * checks detectcolor input to see if the ball is correct color, 
-     * if correct, the command is not run
-     * otherwise if not, the command is run
-     */
     
-    if (colorSensor.detectColor())
+    if (colorSensor.detectColor()) //if color is correct
     {
-      // Shoot the ball, the color is correct or color sensor not work
+      // Shoot the ball
+      if (shooter.isAtTargetSpeed()) {
+        // run feeder
+      }
     }
     else
     {
-      // Regurgitate the ball, the color is incorrect
+
     }
+    //update current # of balls
   }
 
   // Called once the command ends or is interrupted.
@@ -58,7 +60,11 @@ public class Shoot extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    // The command should end when there are no more balls in feeder/shooter
+    // after finished shooting once return true 
+      // if the initial #balls in feeder is == to current #balls in feeder, return false
+    
+    // not a serious comment
+    // 20 minutes of arguing, and all we learned is that we need programmer socks
     return false;
   }
 }
