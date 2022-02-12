44ff3a29ec56cb541e6d03edc951ca21e645a342
stillanitab <803264@seq.org>
2022-02-12 10:38:38 -0800

    adds color sensor code from color sensor branch, imports necessary modules and introduces variables to be used later

--- src/main/java/org/team199/robot2022/subsystems/Shooter.java
+++ src/main/java/org/team199/robot2022/subsystems/Shooter.java
@@ -8,12 +8,22 @@ import org.team199.robot2022.Constants;
 
 //import org.team199.robot2021.Constants;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 //import edu.wpi.first.wpilibj.SpeedController;
 //import java.lang.AutoCloseable;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.lib.MotorControllerFactory;
 //import frc.robot.lib.MotorControllerFactory;
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.util.Color;
+import com.revrobotics.ColorSensorV3;
 import frc.robot.lib.logging.Log;
+
+import com.revrobotics.ColorMatch;
+import com.revrobotics.ColorMatchResult;
+
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Shooter extends SubsystemBase {
     private static double kV = 0.129 / 60;
@@ -29,6 +39,20 @@ public class Shooter extends SubsystemBase {
     private final CANSparkMax slave = MotorControllerFactory.createSparkMax(Constants.DrivePorts.kShooterSlave);
     private final SparkMaxPIDController pidController = master.getPIDController();
 
+    // Adds a box to SmartDashboard, takes in team color from SmartDashboard to check it with color sensor
+    SendableChooser<Character> color = new SendableChooser<>();
+
+    // Team Color is blue by default
+    private char teamColor = 'B';
+
+    // TODO: not sure what we use this for for shooter yet
+    private final int minProximity = 150;
+
+    private final I2C.Port i2cPort = I2C.Port.kOnboard;
+    private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
+    private final ColorMatch m_colorMatcher = new ColorMatch();
+
+    private boolean hasDetectedBall = false;
 
     public Shooter() {
         master.setSmartCurrentLimit(40);
@@ -40,6 +64,15 @@ public class Shooter extends SubsystemBase {
         SmartDashboard.putNumber("Shooter.kV", kV);
         SmartDashboard.putNumber("Shooter.kS", kS);
         
+        if (!m_colorSensor.isConnected()){
+            SmartDashboard.putString("Detected Color", "Error, the color sensor is disconnected");
+        }
+        m_colorMatcher.addColorMatch(Color.kBlue);
+        m_colorMatcher.addColorMatch(Color.kRed);
+
+        color.addOption("Red",'R');
+        color.setDefaultOption("Blue",'B');
+
         slave.follow(master, true);
         master.setInverted(true);
         Log.registerDoubleVar("Spark Max Port 3 Speed", () -> master.getEncoder().getVelocity());
