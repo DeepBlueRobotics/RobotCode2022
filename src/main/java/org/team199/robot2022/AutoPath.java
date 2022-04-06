03eff5d1b2e499da81470eff1f18c0f4b5444fa3
CoolSpy3 <coolspythree@gmail.com>
2022-04-06 16:10:32 -0700

    allow setting shot position in auto paths

--- src/main/java/org/team199/robot2022/AutoPath.java
+++ src/main/java/org/team199/robot2022/AutoPath.java
@@ -3,18 +3,27 @@ package org.team199.robot2022;
 import frc.robot.lib.path.RobotPath;
 import java.util.List;
 
+import org.team199.robot2022.subsystems.Shooter.ShotPosition;
+
 public class AutoPath {
 
     public final boolean shootAtStart;
     public final List<RobotPath> path;
     public final boolean shootAtEnd;
     public final boolean runIntake;
+    public final ShotPosition startShotPosition, endShotPosition;
 
     public AutoPath(boolean shootAtStart, List<RobotPath> path, boolean shootAtEnd, boolean runIntake) {
+        this(shootAtStart, path, shootAtEnd, runIntake, ShotPosition.FENDER, ShotPosition.FENDER);
+    }
+
+    public AutoPath(boolean shootAtStart, List<RobotPath> path, boolean shootAtEnd, boolean runIntake, ShotPosition startShotPosition, ShotPosition endShotPosition) {
         this.shootAtStart = shootAtStart;
         this.path = path;
         this.shootAtEnd = shootAtEnd;
         this.runIntake = runIntake;
+        this.startShotPosition = startShotPosition;
+        this.endShotPosition = endShotPosition;
     }
 
 }
--- src/main/java/org/team199/robot2022/commands/Autonomous.java
+++ src/main/java/org/team199/robot2022/commands/Autonomous.java
@@ -18,6 +18,8 @@ public class Autonomous extends SequentialCommandGroup {
     public Autonomous(AutoPath path, boolean shootAtStart, boolean shootAtEnd, Drivetrain drivetrain, Shooter shooter, IntakeFeeder intakeFeeder) {
         addRequirements(drivetrain, shooter, intakeFeeder);
 
+        if(path.shootAtStart) addCommands(new InstantCommand(() -> shooter.setShotPosition(path.startShotPosition)));
+
         addCommands(
             new InstantCommand(path.path.get(0)::initializeDrivetrainPosition),
             shootAtStart ? new SequentialCommandGroup( new WaitUntilCommand(shooter::isAtTargetSpeed),  new WaitCommand(1.5), new Shoot(intakeFeeder, shooter)) : new InstantCommand()
@@ -26,6 +28,10 @@ public class Autonomous extends SequentialCommandGroup {
         for (int i = 0; i < path.path.size(); i++){
             addCommands(path.path.get(i).getPathCommand(false, i == path.path.size()-1));
         }
+
+
+        if(path.shootAtEnd) addCommands(new InstantCommand(() -> shooter.setShotPosition(path.endShotPosition)));
+
         addCommands(
             shootAtEnd ? new SequentialCommandGroup( new WaitUntilCommand(shooter::isAtTargetSpeed),  new WaitCommand(0.4), new Shoot(intakeFeeder, shooter), new Shoot(intakeFeeder, shooter)) : new InstantCommand()
         );
