9afb1a26de5091c5c1c2549c9f9b9ea987446b69
DriveStationComputer <driverstation@carlmontrobotics.org>
2022-04-08 13:18:12 -0700

    revert intake running in auto

--- src/main/java/org/team199/robot2022/commands/Autonomous.java
+++ src/main/java/org/team199/robot2022/commands/Autonomous.java
@@ -22,7 +22,6 @@ public class Autonomous extends SequentialCommandGroup {
         if(path.shootAtStart) addCommands(new InstantCommand(() -> shooter.setShotPosition(path.startShotPosition)));
 
         addCommands(
-            new InstantCommand(() -> {intakeFeeder.run(Motor.BOTTOM, true);}),
             shootAtStart ? new SequentialCommandGroup( new WaitUntilCommand(shooter::isAtTargetSpeed),  new WaitCommand(1.5), new Shoot(intakeFeeder, shooter)) : new InstantCommand()
         );
         //addCommands(new WaitCommand(4));
